f7a5b1ad96133ab2f20ac9a2f9dbe29da6028ce3
sabanferik <sbnftmaf@gmail.com>
2024-07-05 13:16:13 +0200

    "Updated swagger configuration and host URLs in documents.router.js, swagger.json, and swaggerAutogen.js"

--- src/routes/documents.router.js
+++ src/routes/documents.router.js
@@ -17,16 +17,22 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const redoc = require("redoc-express");
 
+const options = {
+  customCssUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.css",
+  swaggerOptions: { persistAuthorization: true },
+  customJs: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.js",
+  ],
+};
+
 //*swagger
 router.use(
   "/swagger",
   swaggerUi.serve,
-  swaggerUi.setup(require("../../swagger.json"), {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  })
-);
+  swaggerUi.setup(require("../../swagger.json"), options)
+     );
 //* URL => /documents
 //* JSON
 router.use("/json", (req, res) => {
--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -42,7 +42,7 @@ const document = {
     license: { name: packageJson.license },
   },
   // host: `${HOST}:${PORT}`,
-  host: `personnel-api-puzn.onrender.com/`,
+  host: `personnel-api-one.vercel.app//`,
   basePath: "/",
   schemes: ["http", "https"],
   // SimpleToken Settings:
